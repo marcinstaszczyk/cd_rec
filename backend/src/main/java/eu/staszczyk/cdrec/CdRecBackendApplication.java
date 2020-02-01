2221abc0753fa180e8bdd0667a813ae218890900
Marcin Staszczyk <marcin.staszczyk@mc.gov.pl>
2020-02-01 12:59:06 +0100

    Initial Commit: MVN, SpringBoot & Angular CLI generation

---
+++ backend/src/main/java/eu/staszczyk/cdrec/CdRecBackendApplication.java
@@ -0,0 +1,13 @@
+package eu.staszczyk.cdrec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdRecBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdRecBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/eu/staszczyk/cdrec/CdRecBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.staszczyk.cdrec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdRecBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
